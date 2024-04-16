dfd25a34cf5ba478354e1c61521f57153bd5b412
Toby Milner-Gulland <toby@figure.land>
2024-04-16 22:08:11 +0100

    feat: simplified StorageAPI options arguments

--- src/persist.ts
+++ src/persist.ts
@@ -2,13 +2,19 @@ import type { Settable, SettableType } from '.'
 import { isArray } from '@figureland/typekit'
 
 export type StorageAPI<T> = {
-  get: (name: string) => T
-  set: (name: string, data: T) => void
+  get: () => T
+  set: (data: T) => void
+}
+
+export type StorageAPIOptions<T> = {
+  name: PersistenceName
+  validate: LocalStorageValidator
+  fallback: () => T
 }
 
 export type LocalStorageValidator = (v: unknown) => boolean
 
-const getStorageName = (n: string | PersistenceName) => (isArray(n) ? n.join('/') : n)
+export const getStorageName = (n: string | PersistenceName) => (isArray(n) ? n.join('/') : n)
 
 export type PersistenceName = string[]
 
@@ -21,13 +27,13 @@ export type PersistenceOptions<S extends Settable> = {
 
 export const persist = <S extends Settable<any>>(s: S, options: PersistenceOptions<S>) => {
   let lastUpdate: number = performance.now()
-  const existing = options.storage.get(getStorageName(options.name))
+  const existing = options.storage.get()
   if (existing) s.set(s, existing)
 
   s.on((state) => {
     const now = performance.now()
     if (!options.interval || now - lastUpdate >= options.interval) {
-      options.storage.set(getStorageName(options.name), state)
+      options.storage.set(state)
       lastUpdate = now
     }
   })
--- src/typed-local-storage.ts
+++ src/typed-local-storage.ts
@@ -1,20 +1,22 @@
 import { parse, stringify } from 'superjson'
-import type { LocalStorageValidator, StorageAPI } from './persist'
+import { getStorageName, type StorageAPI, type StorageAPIOptions } from './persist'
 
 declare var localStorage: Storage
 
-export const typedLocalStorage = <T>(
-  validate: LocalStorageValidator,
-  fallback: () => T
-): StorageAPI<T> => {
-  const set = (target: string, v: T) => localStorage.setItem(target, stringify(v))
-  const get = (target: string) => {
+export const typedLocalStorage = <T>({
+  name,
+  validate,
+  fallback
+}: StorageAPIOptions<T>): StorageAPI<T> => {
+  const target = getStorageName(name)
+  const set = (v: T) => localStorage.setItem(target, stringify(v))
+  const get = () => {
     const result = parse(localStorage.getItem(target) || '')
     if (validate(result)) {
       return result as T
     } else {
       const v = fallback()
-      set(target, v)
+      set(v)
       return v
     }
   }
