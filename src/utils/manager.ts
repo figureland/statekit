4a271cdc22ccc450660df0ad6a773b2dc95f88a5
Toby Milner-Gulland <toby@figure.land>
2024-04-19 17:17:59 +0100

    feat: allowed multiple arguments for manager.use()

--- src/utils/manager.ts
+++ src/utils/manager.ts
@@ -4,9 +4,12 @@ import { createSubscriptions } from './subscriptions'
 export const manager = () => {
   const subs = createSubscriptions()
 
-  const use = <S extends Disposable>(s: S) => {
-    subs.add(s.dispose)
-    return s
+  const use = <S extends Disposable>(...ds: S[]) => {
+    for (const disposable of ds) {
+      subs.add(disposable.dispose)
+    }
+
+    return ds
   }
 
   const dispose = () => {
