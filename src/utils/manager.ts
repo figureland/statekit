3fc8fe126463af4b8be185ae6fdfdc67e6d6e263
Toby Milner-Gulland <toby@figure.land>
2024-04-19 17:20:43 +0100

    feat: update to manager.use() to revert to previous api

--- src/utils/manager.ts
+++ src/utils/manager.ts
@@ -4,12 +4,9 @@ import { createSubscriptions } from './subscriptions'
 export const manager = () => {
   const subs = createSubscriptions()
 
-  const use = <S extends Disposable>(...ds: S[]) => {
-    for (const disposable of ds) {
-      subs.add(disposable.dispose)
-    }
-
-    return ds
+  const use = <S extends Disposable>(s: S) => {
+    subs.add(s.dispose)
+    return s
   }
 
   const dispose = () => {
