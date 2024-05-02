e08cb676ac6f4caa5a9934c764f8501cbe90f390
Toby Milner-Gulland <toby@figure.land>
2024-05-02 18:26:19 +0100

    feat: added onScopeDispose callback for vue

--- src/vue/use-subscribable.vue.ts
+++ src/vue/use-subscribable.vue.ts
@@ -1,14 +1,17 @@
-import { customRef } from 'vue'
+import { customRef, onScopeDispose } from 'vue'
 import { signal, type UseSignalDependency, type Subscribable } from '@figureland/statekit'
 
 export const useSubscribable = <S>(subscribable: Subscribable<S>) =>
-  customRef<S>((track, set) => ({
-    get: () => {
-      track()
-      return subscribable.get()
-    },
-    set,
-    dispose: subscribable.on(set)
-  }))
+  customRef<S>((track, set) => {
+    onScopeDispose(subscribable.on(set))
+    return {
+      get: () => {
+        track()
+        return subscribable.get()
+      },
+      set,
+      dispose: subscribable.on(set)
+    }
+  })
 
 export const useDerived = <R>(fn: (use: UseSignalDependency) => R) => useSubscribable(signal(fn))
