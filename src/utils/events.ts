92474047a007751340961674cff44a09342efbd1
Toby Milner-Gulland <toby@figure.land>
2024-05-27 13:29:40 +0100

    fix: type issues with events

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -7,11 +7,11 @@ import {
 } from './subscriptions'
 
 export type Events<S extends Record<string, any>, K extends string & keyof S = string & keyof S> = {
-  on: (
-    key: K | '*' | Partial<{ [Key in K]: (eventArg: S[Key]) => void }>,
-    sub?: Subscription<any>
+  on: <Key extends K | '*'>(
+    key: Key | Partial<{ [Key in K]: (eventArg: S[Key]) => void }>,
+    sub?: Key extends '*' ? Subscription<[K, S[K]]> : Subscription<S[Extract<Key, K>]>
   ) => Unsubscribe
-  emit: <Key extends K = K>(key: Key, value: S[Key]) => void
+  emit: <Key extends K>(key: Key, value: S[Key]) => void
   dispose: () => void
   size: () => number
 }
@@ -23,21 +23,18 @@ export const createEvents = <
   S extends Record<string, any>,
   K extends string & keyof S = string & keyof S
 >(): Events<S, K> => {
-  const subs = createTopicSubscriptions()
-  const all = createSubscriptions()
+  const subs = createTopicSubscriptions<K>()
+  const all = createSubscriptions<Subscription<[K, S[K]]>>()
 
   /**
    * Subscribe to a specific event, to all events using '*', or to multiple events
    */
-  const on = (
-    key: K | '*' | Partial<{ [Key in K]: (eventArg: S[Key]) => void }>,
-    sub?: Subscription<any>
-  ): Unsubscribe => {
+  const on: Events<S, K>['on'] = (key, sub?) => {
     if (typeof key === 'string') {
       if (key === '*') {
-        return all.add(sub!)
+        return all.add(sub as Subscription<[K, S[K]]>)
       }
-      return subs.add(key, sub!)
+      return subs.add(key as K, sub as Subscription<S[typeof key]>)
     } else {
       const listeners = key as Partial<{ [Key in K]: (eventArg: S[Key]) => void }>
       const unsubscribes = (entries(listeners) as [K, (eventArg: S[K]) => void][]).map((listener) =>
@@ -54,7 +51,7 @@ export const createEvents = <
 
   return {
     on,
-    emit: <Key extends K = K>(key: Key, value: S[Key]) => {
+    emit: <Key extends K>(key: Key, value: S[Key]) => {
       subs.each(key, value)
       all.each([key, value])
     },
