de0d58e368d62a56c828bba5191ec4dd1915eb7c
Toby Milner-Gulland <toby@figure.land>
2024-06-10 00:14:37 +0100

    fix: minor tweaks to record set behaviour

--- src/record.ts
+++ src/record.ts
@@ -7,11 +7,11 @@ export const record = <R extends Record<string, any>>(
   r: R,
   options?: SignalOptions<R>
 ): SignalRecord<R> => {
-  const parent = signal<R>(() => r, options)
+  const parent = signal<R>(structuredClone(r), options)
   const signals = {} as { [K in keyof R]: Signal<R[K]> }
 
   for (const k in r) {
-    signals[k] = signal(() => r[k])
+    signals[k] = signal(r[k])
     parent.use(signals[k].on(() => parent.set(getObject())))
     parent.use(signals[k].dispose)
   }
@@ -28,8 +28,8 @@ export const record = <R extends Record<string, any>>(
 
   const set = (v: R | Partial<R> | ((v: R) => R | Partial<R>), sync: boolean = true): void => {
     const u = isFunction(v) ? (v as (v: R) => R)(parent.get()) : v
-    for (const k in v) {
-      key(k).set(u[k] as R[typeof k], sync)
+    for (const k of keys(v)) {
+      key(k)?.set(u[k] as R[typeof k], sync)
     }
   }
 
